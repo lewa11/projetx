e99287754b950ccef787ccca3e798f994f51e0f2
Wahid Amar <wahid.amar@gmail.com>
2024-09-04 00:20:27 +0200

    Update dependencies and add contact and language components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LanguageComponent } from './pages/language/language.component';
 
 
 export const routes: Routes = [
     { path:"" , component: HomeComponent },
+    { path:"app-contact" , component: ContactComponent },
+    { path:"app-language" , component: LanguageComponent },
+
+    
     { path:"**", redirectTo: "/" }
 
 ];
